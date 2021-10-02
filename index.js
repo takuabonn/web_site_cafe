ce92fde7aca81c4c8d4009681a5f6347d9615d96
takumi <k.takumi19980322@gmail.com>
2021-10-02 15:50:02 +0900

    jsで動作付

--- index.js
+++ index.js
@@ -11,14 +11,48 @@ const onClick = () => {
         console.log(nav_list.style)
     }
     btn_line.classList.toggle("open")
-        // body.classList.toggle("fixed")
 }
 btn.addEventListener("click", onClick);
 
 document.querySelectorAll(".nav-list-item").forEach((item) => {
     item.addEventListener("click", () => {
         nav_list.style.right = -150 + "px";
-        // body.classList.remove("fixed")
         btn_line.classList.remove("open")
     })
+})
+
+const elements = document.querySelectorAll(".sc")
+window.addEventListener('scroll', () => {
+    elements.forEach((ele) => {
+        const target_position = ele.getBoundingClientRect().top;
+        if (window.pageYOffset > ((target_position + window.pageYOffset) - window.innerHeight) + 100) {
+            ele.classList.add("show")
+        }
+
+        if (window.pageYOffset === 0) {
+            ele.classList.remove("show")
+        }
+    })
+});
+
+let current = 1
+const slideshow = () => {
+
+    const header_images = document.querySelectorAll(".header-image");
+
+    if (current === 2) {
+        current = 0;
+        header_images[current].classList.add("active");
+        header_images[2].classList.remove("active");
+    } else {
+        current++;
+        header_images[current].classList.add("active");
+        header_images[current - 1].classList.remove("active");
+
+    }
+}
+
+window.addEventListener('load', () => {
+    slideshow();
+    setInterval(slideshow, 5000)
 })
